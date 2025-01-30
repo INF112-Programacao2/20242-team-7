f91ae03cffd50593e847e4730f30e115f2eb8b0e
PatrickSN <86534143+PatrickSN@users.noreply.github.com>
2025-01-29 22:34:37 -0300

    efeito piloto

--- include/Piloto.h
+++ include/Piloto.h
@@ -7,13 +7,15 @@
 class Unidade;
 
 class Piloto : public Carta{
-    private:
+private:
     std:: string efeito_piloto;
     Unidade unidade_tripulada;
-    public:
+public:
     Piloto(std::string _efeito_piloto, Unidade Unidade, std::string _Desc, std::string _Tipo, std::string _Nome, int _Custo, int _ID );
     ~Piloto();
+    void le();
     void fornece_efeito(Jogador& jog);
+    void setUnidade(Unidade _unidade);
     Unidade getUnidade();
     
 };
